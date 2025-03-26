a476547744d387f9f88a202ba97d2f1a22f860a2
FuriousNayan <npatel24@my.waketech.edu>
2025-03-26 12:58:49 -0400

    Fixing

--- validationScript.js
+++ validationScript.js
@@ -1,19 +1,14 @@
-window.onload = function() {
-  document.getElementById("myForm").addEventListener("submit", (event) => {
-      event.preventDefault();
+document.getElementById("submit").addEventListener("click", (event) => {
 
-    const fieldValue = document.getElementById("inputField")
+  const fieldValue = document.getElementById("inputField")
 
-    const alphanumericInput = /^[a-zA-Z0-9]+$/;
+  const alphanumericInput = /^[a-zA-Z0-9]+$/;
 
-    if (alphanumericInput.test(fieldValue.value)) {
-      fieldValue.setCustomValidity("");
-      alert("Form submitted successfully!");
-      document.getElementById("myForm").submit();
-    } else {
-      fieldValue.setCustomValidity("Please enter only alphanumeric characters.");
-    }
-  });
-};
-
-//
+  if (alphanumericInput.test(fieldValue.value)) {
+    fieldValue.setCustomValidity("");
+    alert("Form submitted successfully!");
+    document.getElementById("myForm").submit();
+  } else {
+    fieldValue.setCustomValidity("Please enter only alphanumeric characters.");
+  }
+});
